81192bc3efb3d346b3e2b47b6d5facb027bc5dc8
Koxx12 is bad <90344513+koxx12-dev-is-bad@users.noreply.github.com>
2022-11-10 13:29:49 -0500

    improve battery module

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-killall -q polybar
+# killall -q polybar
 # If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
+polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-# polybar main for desktop and polybar laptop for laptop
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar laptop 2>&1 | tee -a /tmp/polybar.log & disown
